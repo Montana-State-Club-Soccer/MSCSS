62d52c363dd04537e95294ff0cd6216832672d84
Seth Keirn <118314693+sethorater@users.noreply.github.com>
2025-12-03 18:02:38 -0700

    ImageBlock Build

---
+++ dist/stories/ImageBlock.stories.d.ts
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { ImageBlock } from '../components/ImageBlock';
+declare const meta: Meta<typeof ImageBlock>;
+export default meta;
+type Story = StoryObj<typeof ImageBlock>;
+export declare const Basic: Story;
+export declare const WithTitleAndDescription: Story;
+export declare const SecondaryVariant: Story;
+export declare const PlaceholderImage: Story;
+export declare const WideAspect: Story;
+export declare const TallAspect: Story;
+//# sourceMappingURL=ImageBlock.stories.d.ts.map
